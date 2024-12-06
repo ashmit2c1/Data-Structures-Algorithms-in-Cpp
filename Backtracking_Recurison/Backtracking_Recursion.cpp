dc3b6b1dc23e64cf14ff045919b16f6445147d17
ashmit2c1 <codeatashmit@gmail.com>
2024-12-06 18:03:03 +0530

    Backtracking and Recursion

--- Backtracking_Recurison/Backtracking_Recursion.cpp
+++ Backtracking_Recurison/Backtracking_Recursion.cpp
@@ -133,3 +133,99 @@ int findMinPath(vector<vector<int>>&matrix){
     int ans=findMinHelper(n-1,m-1,matrix);
     return ans;
 }
+// WORD SEARCH
+bool solveFunction(vector<vector<char>>&board,int i,int j,int index,string word){
+    if(index==word.size()){
+        return true;
+    }
+    if(i<0 || i>=board.size() || j<0 || j>=board[0].size()){
+        return false;
+    }
+    if(board[i][j]!=word[index]){
+        return false;
+    }
+    char temp = board[i][j];
+    board[i][j]='#';
+    bool case1=solveFunction(board,i+1,j,index+1,word);
+    bool case2=solveFunction(board,i,j+1,index+1,word);
+    bool case3=solveFunction(board,i-1,j,index+1,word);
+    bool case4 = solveFunction(board,i,j-1,index+1,word);
+    board[i][j]=temp;
+    bool ans = case1  || case2 || case3 ||case4;
+    return ans;
+
+}
+bool exist(vector<vector<char>>&board,string word){
+    for(int i=0;i<board.size();i++){
+        for(int j=0;j<board[0].size();j++){
+            if(solveFunction(board,i,j,0,word)==true){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+// COMBINATION SUM 1 
+void combinationSum1Helper(vector<vector<int>>&ans,vector<int>&current,int index,vector<int>&arr,int target){
+    if(target==0){
+        ans.push_back(current);
+        return;
+    }
+    if(target<0){
+        return;
+    }
+    for(int i=index;i<arr.size();i++){
+        current.push_back(arr[i]);
+        combinationSum1Helper(ans,current,i,arr,target-arr[i]);
+        current.pop_back();
+    }
+}
+vector<vector<int>>combinationSum1(vector<int>&arr,int target){
+    int n=arr.size();
+    vector<vector<int>>ans;
+    vector<int>current;
+    combinationSum1Helper(ans,current,0,arr,target);
+    return ans;
+}
+// COMBINATION SUM 2 
+void combinationSum2Helper(vector<vector<int>>&ans,vector<int>&arr,int target,int index,vector<int>&current){
+    if(target==0){
+        ans.push_back(current);
+        return;
+    }
+    if(target<0){
+        return;
+    }
+    for(int i=index;i<arr.size();i++){
+        if(i>index && arr[i]==arr[i-1]){
+            continue;
+        }
+        current.push_back(arr[i]);
+        combinationSum2Helper(ans,arr,target-arr[i],i+1,current);
+        current.pop_back();
+    }
+}
+vector<vector<int>>combinationSum2(vector<int>&arr,int target){
+    vector<vector<int>>ans;
+    vector<int>current;
+    sort(arr.begin(),arr.end());
+    combinationSum2Helper(ans,arr,target,0,current);
+    return ans;
+}
+// UNIQUE PATHS ( WITHOUT OBSTACLES )
+int countPaths(int i,int j,int m,int n){
+    if(i>=m || j>=n){
+        return 0;
+    }
+    if(i==m-1 && j==n-1){
+        return 1;
+    }
+    int rightpath=countPaths(i,j+1,m,n);
+    int leftpath=countPaths(i+1,j,m,n);
+    return rightpath+leftpath;
+}
+int uniquePaths(int m,int n){
+    int ans=countPaths(0,0,m,n);
+    return ans;
+}
+// UNIQUE PATHS ( WITH OBSTACLES)
